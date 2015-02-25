9e698b59eef4f671405a704e332b52534551f946
zhangtiny123 <tzhang920314@gmail.com>
2015-02-25 16:22:29 +0800

    add pos test for more than one count

--- src/test/java/com/homework/pos/PosTest.java
+++ src/test/java/com/homework/pos/PosTest.java
@@ -4,13 +4,12 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.mockito.Mockito.mock;
 
 public class POSTest
 {
 
     @Test
-    public void should_return_detail_list_when_given_cart_list()
+    public void should_return_receipt_list_when_given_cart_with_one_product()
     {
         // given
         POS pos = new POS();
@@ -24,5 +23,18 @@ public class POSTest
         assertThat(receipt.getTotal(), is(495.0));
     }
 
+    @Test
+    public void should_return_receipt_when_given_cart_with_one_product_but_more_than_one_count()
+    {
+        // given
+        POS pos = new POS();
+        Cart cart = new Cart();
+        cart.add(new Item(new Good("ITEM000003", 70), 12));
 
+        // when
+        Receipt receipt = pos.settle(cart);
+
+        //then
+        assertThat(receipt.getTotal(), is(840.0));
+    }
 }
